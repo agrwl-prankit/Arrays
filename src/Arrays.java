362f229d9bd8cd7110addd37467ddb1eede21737
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-04-29 21:32:00 +0530

    Initial commit

---
+++ src/Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+
+    int[] arr = new int[5];
+    int size = -1;
+
+    public void add(int data){
+
+    }
+
+    public boolean isEmpty(){
+        return size == -1;
+    }
+
+    public boolean isFull(){
+        return size == arr.length;
+    }
+
+}
